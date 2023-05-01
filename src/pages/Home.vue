678cf940aed54ff422d9a9cbc8ca0c03832622aa
Davitpurceladze <purceladzedavit@gmail.com>
2023-05-01 19:09:40 +0400

    add assets and create home page

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,11 +1,43 @@
 <template>
-  <h1>Home page</h1>
+  <div class="mainContainer">
+    <div>
+      <img src="../assets/Vector.png" alt="company logo" class="logo">
+    </div>
+    <RouterLink to="/personal" class="start-btn" >კითხვარის <br/> დაწყება</RouterLink>
+  </div>
 </template>
 
 <script setup>
 
+ 
+
 </script>
 
 <style scoped>
+.mainContainer {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  gap: 101px;
+  justify-content: center;
+  align-items: center;
+}
+
+ 
+
+.start-btn{
+  font-size: 30px;
+  line-height: 36px;
+  font-weight: 700;
+  color: var(--darkText);
+  text-align: center;
+  text-decoration: none;
+}
+
+.start-btn:hover{
+  cursor: pointer;
+  
+}
 
 </style>
--- src/router/router.js
+++ src/router/router.js
@@ -1,11 +1,17 @@
 import {createRouter, createWebHistory} from 'vue-router'
 import Home from '../pages/Home.vue'
+import Personal from '../pages/Personal.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: Home 
+  },
+  {
+    path: '/personal',
+    name: 'personal',
+    component: Personal
   }
 ]
 
